fbcc2691ac159cc5c51701ca95ae64e6ab19f7ca
nishat2017 <ntb017ntb@gmail.com>
2025-08-10 08:48:35 +0600

    feat: Add complete refactored auth system

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
+
+# app.py
+
 from flask import Flask
+from dotenv import load_dotenv
+import os
+from routes.auth import auth_bp
+load_dotenv()
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('FLASK_SECRET_KEY', 'a-fallback-secret-key')
+app.register_blueprint(auth_bp)
 
-@app.route("/")
-def Nomad_Co():
-    return "<p>Hello,Everyone This is the Nomad Co. App</p>"
+if __name__ == '__main__':
+    app.run(debug=True, port=5001)
