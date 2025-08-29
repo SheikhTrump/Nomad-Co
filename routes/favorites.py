5649bea3e4c0d75d6812909442f817af6aaa3403
SheikhTrump <onlywebs3500@gmail.com>
2025-08-29 18:24:43 +0600

    Updated User['user_id']

--- routes/favorites.py
+++ routes/favorites.py
@@ -3,47 +3,56 @@
 from flask import Blueprint, request, redirect, url_for, session, flash, render_template
 from models.favorites import add_favorite_to_user, remove_favorite_from_user, get_user_favorite_spaces
 from models.space import get_space_by_id, get_popular_spaces_in_location
+from models.user import db
+from bson.objectid import ObjectId
 
 favorites_bp = Blueprint('favorites', __name__, template_folder='../templates')
 
+def get_current_user():
+    if 'user_id' in session:
+        try:
+            return db.users.find_one({'_id': ObjectId(session['user_id'])})
+        except Exception:
+            return None
+    return None
+
 @favorites_bp.route('/favorites')
 def show_favorites():
-    if 'user_id' not in session:
+    user = get_current_user()
+    if not user:
         flash('You need to be logged in to see your favorites.', 'warning')
         return redirect(url_for('auth.login'))
-    
-    favorite_spaces = get_user_favorite_spaces(session['user_id'])
-    
-    # This will be displayed on the favorites page itself after an action
+
+    favorite_spaces = get_user_favorite_spaces(user['user_id'])
     suggested_spaces = session.pop('suggested_spaces', None)
 
     return render_template('favorites.html', favorites=favorite_spaces, suggestions=suggested_spaces)
 
 @favorites_bp.route('/favorite/add/<space_id>')
 def add_favorite(space_id):
-    if 'user_id' not in session:
+    user = get_current_user()
+    if not user:
         return redirect(url_for('auth.login'))
-    
-    add_favorite_to_user(session['user_id'], space_id)
+
+    add_favorite_to_user(user['user_id'], space_id)
     flash('Added to your favorites!', 'success')
 
     space = get_space_by_id(space_id)
     if space:
         suggestions = get_popular_spaces_in_location(space['location_city'], exclude_id=space_id)
         session['suggested_spaces'] = suggestions
-        # Set a flag to make the dashboard icon blink
         session['new_suggestions'] = True
 
     return redirect(request.referrer or url_for('space_filters.view_spaces'))
 
 @favorites_bp.route('/favorite/remove/<space_id>')
 def remove_favorite(space_id):
-    if 'user_id' not in session:
+    user = get_current_user()
+    if not user:
         return redirect(url_for('auth.login'))
-    
+
     space = get_space_by_id(space_id)
-    
-    remove_favorite_from_user(session['user_id'], space_id)
+    remove_favorite_from_user(user['user_id'], space_id)
     flash('Removed from your favorites.', 'info')
 
     if space:
